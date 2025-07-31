7b0d1a723a0319f32e874752056252c23113a5e9
Sesha Sravani <sraavz.aish@gmail.com>
2025-07-31 16:42:17 -0400

    Added Dietician Get By ID

---
+++ src/test/java/com/CalorieHackers_StepDefinition/DieticianGetByID.java
@@ -0,0 +1,29 @@
+package com.CalorieHackers_StepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DieticianGetByID {
+	
+	
+	
+
+@Given("Admin create GET request")
+public void admin_create_get_request() {
+    
+}
+
+@When("Admin send GET http request with endpoint")
+public void admin_send_get_http_request_with_endpoint() {
+    
+}
+
+@Then("Admin receives 200 ok with details of the dietician id")
+public void admin_receives_ok_with_details_of_the_dietician_id() {
+   
+}
+
+
+
+}
