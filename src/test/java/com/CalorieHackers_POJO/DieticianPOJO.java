5ee728c7ab709ee38232dbd5f71a64a7ce48668d
Sesha Sravani <sraavz.aish@gmail.com>
2025-08-01 16:55:15 -0400

    Dirtician GetById positive

--- src/test/java/com/CalorieHackers_POJO/DieticianPOJO.java
+++ src/test/java/com/CalorieHackers_POJO/DieticianPOJO.java
@@ -1,25 +1,43 @@
-//package com.CalorieHackers_POJO;
-//
-//public class Dietitian {
-//
-//}
-
 package com.CalorieHackers_POJO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class DieticianPOJO {
 
     private int id;
+
+    @JsonProperty("loginPassword")
     private String loginPassword;
-    private String Firstname;
-    private String Lastname;
-    private String ContactNumber;
-    private String DateOfBirth;
-    private String Email;
-    private String HospitalName;
-    private String HospitalStreet;
-    private String HospitalCity;
-    private String HospitalPincode;
-    private String Education;
+
+    @JsonProperty("Firstname")
+    private String firstname;
+
+    @JsonProperty("Lastname")
+    private String lastname;
+
+    @JsonProperty("ContactNumber")
+    private String contactNumber;
+
+    @JsonProperty("DateOfBirth")
+    private String dateOfBirth;
+
+    @JsonProperty("Email")
+    private String email;
+
+    @JsonProperty("HospitalName")
+    private String hospitalName;
+
+    @JsonProperty("HospitalStreet")
+    private String hospitalStreet;
+
+    @JsonProperty("HospitalCity")
+    private String hospitalCity;
+
+    @JsonProperty("HospitalPincode")
+    private String hospitalPincode;
+
+    @JsonProperty("Education")
+    private String education;
 
     // Getters & Setters
     public int getId() {
@@ -37,73 +55,72 @@ public class DieticianPOJO {
     }
 
     public String getFirstname() {
-        return Firstname;
+        return firstname;
     }
     public void setFirstname(String firstname) {
-        Firstname = firstname;
+        this.firstname = firstname;
     }
 
     public String getLastname() {
-        return Lastname;
+        return lastname;
     }
     public void setLastname(String lastname) {
-        Lastname = lastname;
+        this.lastname = lastname;
     }
 
     public String getContactNumber() {
-        return ContactNumber;
+        return contactNumber;
     }
     public void setContactNumber(String contactNumber) {
-        ContactNumber = contactNumber;
+        this.contactNumber = contactNumber;
     }
 
     public String getDateOfBirth() {
-        return DateOfBirth;
+        return dateOfBirth;
     }
     public void setDateOfBirth(String dateOfBirth) {
-        DateOfBirth = dateOfBirth;
+        this.dateOfBirth = dateOfBirth;
     }
 
     public String getEmail() {
-        return Email;
+        return email;
     }
     public void setEmail(String email) {
-        Email = email;
+        this.email = email;
     }
 
     public String getHospitalName() {
-        return HospitalName;
+        return hospitalName;
     }
     public void setHospitalName(String hospitalName) {
-        HospitalName = hospitalName;
+        this.hospitalName = hospitalName;
     }
 
     public String getHospitalStreet() {
-        return HospitalStreet;
+        return hospitalStreet;
     }
     public void setHospitalStreet(String hospitalStreet) {
-        HospitalStreet = hospitalStreet;
+        this.hospitalStreet = hospitalStreet;
     }
 
     public String getHospitalCity() {
-        return HospitalCity;
+        return hospitalCity;
     }
     public void setHospitalCity(String hospitalCity) {
-        HospitalCity = hospitalCity;
+        this.hospitalCity = hospitalCity;
     }
 
     public String getHospitalPincode() {
-        return HospitalPincode;
+        return hospitalPincode;
     }
     public void setHospitalPincode(String hospitalPincode) {
-        HospitalPincode = hospitalPincode;
+        this.hospitalPincode = hospitalPincode;
     }
 
     public String getEducation() {
-        return Education;
+        return education;
     }
     public void setEducation(String education) {
-        Education = education;
+        this.education = education;
     }
 }
-
